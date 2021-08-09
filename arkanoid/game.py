eb64fadfdaba7150c61c0e28b0a0833eb238db44
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-08-09 18:08:27 +0200

    update

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -9,17 +9,28 @@ ball = Actor('ball')
 ball.pos = (WIDTH / 2, HEIGHT / 2)
 ball.dx = 1
 ball.dy = -1
+ball.speed = 5
+
 
 def update():
-    ball.x = ball.x + ball.dx
-    ball.y = ball.y + ball.dy
+    ball.x = ball.x + ball.speed * ball.dx
+    ball.y = ball.y + ball.speed * ball.dy
 
+    # odrazenie zhora
     if ball.top <= 0:
         ball.dy *= -1
 
+    # odrazenie zprava
     if ball.right >= WIDTH:
         ball.dx *= -1
 
+    # odrazenie zdola
+    if ball.bottom >= HEIGHT:
+        ball.dy *= -1
+
+    # odrazenie zlava
+    if ball.left <= 0:
+        ball.dx *= -1
 
 def draw():
     screen.clear()
