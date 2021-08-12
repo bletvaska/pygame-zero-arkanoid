0ce9420e18a6bf71e6d393b76d8f8c17857eb6b0
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-08-12 18:43:25 +0200

    lopticka sa zobrazuje na zaklade triedy Ball

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -3,6 +3,11 @@ WIDTH = 640
 HEIGHT = 480
 TITLE = "arkanoid.py"
 
+class Brick(Actor):
+    def __init__(self):
+        super().__init__('brick.purple')  # Actor('brick.purple')
+
+
 class Ball(Actor):
     def __init__(self):
         super().__init__('ball')  # Actor('ball')
@@ -30,16 +35,10 @@ class Ball(Actor):
             self.dx *= -1
             self.left = 0
 
-ballx = Ball()
+        #if self.colliderect(xxx):
+        #    yyy.remove(xxx)
 
-
-ball = Actor("ball")
-# ball.x = WIDTH / 2
-# ball.y = HEIGHT / 2
-ball.pos = (WIDTH / 2, HEIGHT / 2)
-ball.dx = 1
-ball.dy = -1
-ball.speed = 5
+ball = Ball()
 
 paddle = Actor("paddle")
 paddle.bottom = HEIGHT
@@ -56,25 +55,7 @@ for col in range(10):
 
 
 def update():
-    ballx.update()
-
-    ball.x = ball.x + ball.speed * ball.dx
-    ball.y = ball.y + ball.speed * ball.dy
-
-    # odrazenie zhora
-    if ball.top <= 0:
-        ball.dy *= -1
-        ball.top = 0
-
-    # odrazenie zprava
-    if ball.right >= WIDTH:
-        ball.dx *= -1
-        ball.right = WIDTH
-
-    # odrazenie zlava
-    if ball.left <= 0:
-        ball.dx *= -1
-        ball.left = 0
+    ball.update()
 
     # god mode
     paddle.x = ball.x
@@ -117,11 +98,9 @@ def update():
 
 
 def draw():
-    #screen.clear()
     screen.blit('background', (0, 0))
     ball.draw()
     paddle.draw()
-    ballx.draw()
 
     for brick in bricks:
         brick.draw()
